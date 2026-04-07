555750d4b64ab900dcbe5526b3f742ee7de1ee92
Isaname888 <alexanderalish0@gmail.com>
2026-04-07 08:45:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function changeLang(lang){
+
+if(lang=='ru'){
+document.getElementById('title').innerHTML='Шахматный клуб Черный слон'
+document.getElementById('subtitle').innerHTML='Место где развивается стратегия и интеллект'
+}
+
+if(lang=='en'){
+document.getElementById('title').innerHTML='Black Elephant Chess Club'
+document.getElementById('subtitle').innerHTML='Place where strategy grows'
+}
+
+}
